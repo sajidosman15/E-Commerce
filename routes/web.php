20b5e97b34018a808cb0866a0cd4d3c894f48944
Sajid <muhammad35-2660@diu.edu.bd>
2022-01-09 11:31:13 +0600

    remove track order

--- routes/web.php
+++ routes/web.php
@@ -65,8 +65,8 @@ Route::get('/product-grids','FrontendController@productGrids')->name('product-gr
 Route::get('/product-lists','FrontendController@productLists')->name('product-lists');
 Route::match(['get','post'],'/filter','FrontendController@productFilter')->name('shop.filter');
 // Order Track
-Route::get('/product/track','OrderController@orderTrack')->name('order.track');
-Route::post('product/track/order','OrderController@productTrackOrder')->name('product.track.order');
+// Route::get('/product/track','OrderController@orderTrack')->name('order.track');
+// Route::post('product/track/order','OrderController@productTrackOrder')->name('product.track.order');
 // Blog
 Route::get('/blog','FrontendController@blog')->name('blog');
 Route::get('/blog-detail/{slug}','FrontendController@blogDetail')->name('blog.detail');
