db6efe3ff6032a1b7e0983d23418b1fb9ebb5ffd
Sajid <muhammad35-2660@diu.edu.bd>
2022-01-09 12:45:17 +0600

    header fix

--- resources/views/frontend/layouts/head.blade.php
+++ resources/views/frontend/layouts/head.blade.php
@@ -1,5 +1,7 @@
 <!-- Meta Tag -->
 @yield('meta')
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <!-- Title Tag  -->
 <title>@yield('title')</title>
 <!-- Favicon -->
