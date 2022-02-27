73dde8ece9df0979486164f862f184db95831ef5
merazul2755 <merazulehsan880@gmail.com>
2022-02-27 12:34:25 +0600

    admin panel design modified

--- resources/views/backend/order/show.blade.php
+++ resources/views/backend/order/show.blade.php
@@ -32,8 +32,8 @@
             <td>{{$order->first_name}} {{$order->last_name}}</td>
             <td>{{$order->email}}</td>
             <td>{{$order->quantity}}</td>
-            <td>@foreach($shipping_charge as $data) $ {{number_format($data,2)}} @endforeach</td>
-            <td>${{number_format($order->total_amount,2)}}</td>
+            <td>@foreach($shipping_charge as $data) BDT. {{number_format($data,2)}} @endforeach</td>
+            <td>BDT.{{number_format($order->total_amount,2)}}</td>
             <td>
                 @if($order->status=='new')
                   <span class="badge badge-primary">{{$order->status}}</span>
@@ -93,15 +93,15 @@
                           $shipping_charge=DB::table('shippings')->where('id',$order->shipping_id)->pluck('price');
                       @endphp
                         <td>Shipping Charge</td>
-                        <td> : $ {{number_format($shipping_charge[0],2)}}</td>
+                        <td> : BDT. {{number_format($shipping_charge[0],2)}}</td>
                     </tr>
                     <tr>
                       <td>Coupon</td>
-                      <td> : $ {{number_format($order->coupon,2)}}</td>
+                      <td> : BDT. {{number_format($order->coupon,2)}}</td>
                     </tr>
                     <tr>
                         <td>Total Amount</td>
-                        <td> : $ {{number_format($order->total_amount,2)}}</td>
+                        <td> : BDT. {{number_format($order->total_amount,2)}}</td>
                     </tr>
                     <tr>
                         <td>Payment Method</td>
@@ -180,7 +180,7 @@
                   </tr>
                   <tr class="borderClass">
                     <td>Amount</td>
-                    <td> : {{$product->amount[0]->amount}}</td>
+                    <td> :BDT. {{$product->amount[0]->amount}}</td>
                   </tr>
                 @endforeach   
               </table>
